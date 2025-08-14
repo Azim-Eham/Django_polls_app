8e6180cdd037f8f2e3152526cac1e2d44bd84611
Azim-Eham <azimeham84@gmail.com>
2025-08-15 02:07:21 +0600

    created polls app

---
+++ polls/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world, your are at polls index.")
